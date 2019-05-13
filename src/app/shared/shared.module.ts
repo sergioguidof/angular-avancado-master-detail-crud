b2d17f7e00a60d0db13feda3dc7e3597831ba668
Sergio <sergio.guidof@gmail.com>
2019-05-13 13:47:15 -0300

    project restructuring

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
