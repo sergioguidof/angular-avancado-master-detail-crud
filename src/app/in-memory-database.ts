8aecf3ab459f06ce514679296e6a1f16e018afdf
Sergio <sergio.guidof@gmail.com>
2019-05-20 11:07:09 -0300

    improving reports

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -13,9 +13,9 @@ export class InMemoryDatabase implements InMemoryDbService {
         ];
 
         const entries: Entry[] = [
-            {id: 1, name: "Gás de Cozinha", description: "asdasda", type: "expense", amount: "80,00", date: "11/11/2011", paid: true, categoryId: categories[1].id, category: categories[1]} as Entry,
-            {id: 2, name: "Gás de Cozinha", description: "asdasda", type: "revenue", amount: "80,00", date: "11/11/2011", paid: false, categoryId: categories[1].id, category: categories[1]} as Entry,
-            {id: 3, name: "Gás de Cozinha", description: "asdasda", type: "expense", amount: "80,00", date: "11/11/2011", paid: true, categoryId: categories[1].id, category: categories[1]} as Entry,
+            {id: 1, name: "Gás de Cozinha", description: "asdasda", type: "expense", amount: "80,00", date: "11/11/2018", paid: true, categoryId: categories[1].id, category: categories[1]} as Entry,
+            {id: 2, name: "Gás de Cozinha", description: "asdasda", type: "revenue", amount: "80,00", date: "11/11/2018", paid: false, categoryId: categories[1].id, category: categories[1]} as Entry,
+            {id: 3, name: "Gás de Cozinha", description: "asdasda", type: "expense", amount: "80,00", date: "11/11/2018", paid: true, categoryId: categories[1].id, category: categories[1]} as Entry,
         ];
 
         return {categories, entries}
